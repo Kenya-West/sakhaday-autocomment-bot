dcc515e719ad725fb27b41d4598126f39080088a
Innokenty Ivanov <Kenya-West@outlook.com>
2023-05-09 18:26:48 +0600

    Add basic logic

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,50 @@
-// eslint-disable-next-line no-console
-console.log("Hello world!");
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import * as dotenv from "dotenv";
+import { Telegraf } from "telegraf";
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+const port = process.env.PORT || 3000;
+const token = process.env.TG_TOKEN!;
+const channelId = process.env.TG_CHANNEL!;
+let isWorking = false;
+
+const bot = new Telegraf(token);
+
+void bot.launch();
+
+bot.command("start", (ctx) => {
+  if (isWorking === false) {
+    isWorking = true;
+    void ctx.telegram.sendMessage(ctx.chat.id, `${process.env.TG_START_MESSAGE ?? "Started"}`, {});
+  } else {
+    void ctx.telegram.sendMessage(ctx.chat.id, `Я уже в статусе: ${process.env.TG_START_MESSAGE ?? "Started"}`, {});
+  }
+});
+
+bot.command("stop", (ctx) => {
+  if (isWorking === true) {
+    isWorking = false;
+    void ctx.telegram.sendMessage(ctx.chat.id, `${process.env.TG_STOP_MESSAGE ?? "Stopped"}`, {});
+  } else {
+    void ctx.telegram.sendMessage(ctx.chat.id, `Я уже в статусе: ${process.env.TG_STOP_MESSAGE ?? "Stopped"}`, {});
+  }
+});
+
+bot.command("status", (ctx) => {
+  void ctx.telegram.sendMessage(
+    ctx.chat.id,
+    `Я уже в статусе: ${isWorking ? process.env.TG_START_MESSAGE ?? "Started" : process.env.TG_STOP_MESSAGE ?? "Stopped"}`,
+    {}
+  );
+});
+
+bot.on("message", (ctx) => {
+  // eslint-disable-next-line eqeqeq
+  if (isWorking && ctx.message.sender_chat?.id == (process.env.TG_CHANNEL as unknown as number)) {
+    void ctx.telegram.sendMessage(ctx.chat.id, `${process.env.TG_COMMENT_TEXT ?? "Lorem ipsum"}`, {
+      reply_to_message_id: ctx.message.message_id
+    });
+  }
+});
